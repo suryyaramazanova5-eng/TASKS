533ebf09b1e5f4a773a6f50fd72a3b0c3928867a
suryyaramazanova5-eng <suryyaramazanova5@gmail.com>
2025-11-08 11:33:26 +0400

    Javascript

---
+++ task 17 js/app.js
@@ -0,0 +1,21 @@
+//task 1
+ //console.log("Hello world");
+
+
+ //task 2
+ //const name = "Sureyya";
+ 
+
+ //task 3
+ //let 2name = "Sureyya"   evvelde reqem gele bilmir
+// let c@rt = "Sureyya"    ancaq _ ,$ isarelerinden istifade etmek olar
+
+// let person = " Sureyya" duzdur
+// let title color = " Sureyya "  ad vererken bosluq olmaz
+// let console = " Sureyya "  ad kimi properti yazmaq olmaz 
+// let $add = " Sureyya" duzdur
+
+//task 4
+//console.log(prompt("Ad soyad"));
+
+ 
