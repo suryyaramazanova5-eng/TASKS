76a95a0f8318b486075c3e6c9cbc38de701db612
suryyaramazanova5-eng <suryyaramazanova5@gmail.com>
2025-11-22 10:38:27 +0400

    js

---
+++ task 19 js/bp.js
@@ -0,0 +1,115 @@
+//task 1 
+
+//let num = +prompt(" Iki reqemli eded daxil et"){
+ //while (num<100)
+  //num+=7
+//if(num>=100){
+  //console.log(num);
+  
+//}
+//}
+
+
+//task 2
+//let str = Number(prompt(" say daxil et"));
+//for (let i =0; i<=str;i++){
+  //  console.log("I know how to use cycles");
+    
+//}
+
+//task 3
+//for( let i = 100 ; i<1000; i++){
+   // if( i%10==0){
+       // console.log(i);
+        
+   // }
+//}
+
+//taks 4
+//let sum = 0
+ //for( let i = 10 ; i<100; i++){
+   // if( i%2!==0){
+       // sum += i;
+        
+   // }
+ //}
+ //console.log(sum);
+
+
+ //task 5
+ //let sum = 0
+ //let num = Number(prompt(" Eded daxil edin"));
+  //for ( let i = 100; i<1000; i++){
+//  if( i%num==0){
+  //     sum+=i
+    //}
+//}
+ // console.log(sum );
+  
+
+
+ //task 6
+ //let n = Number(prompt(" N sayi daxil edin"));
+// let isPrime = true;
+ //for( let i = n; i >1; i--){
+
+   // for ( let j = 2 ; j<Math.floor(i/2); j++){
+       // if( i%j==0){
+           // isPrime=false;
+        //break;
+   // }
+ //}
+   // if(isPrime)console.log(i);
+ 
+//}
+ 
+ 
+
+
+ //task 7
+ //let hasil = 1 
+
+ //for ( let i = 1 ; i<10; i++){
+   //for( let j = 1; j<10; j++){
+    //console.log(`${i} x ${j}=${i * J}`);
+    
+  // }
+
+ //}
+
+
+ //task 8
+
+
+ //let str = prompt(" Metn daxil edin:");
+ //let reverse = "";
+
+    //for(let i  = str.length -1;i>=0;i--){
+      //reverse += str[i];
+      
+    //}
+    //console.log(reverse);
+
+ //task 9
+ //let str = " Hello Wor.ld"
+ //for (let i = 0; i < str.length; i++) {
+     // if(str[i]=="."){
+       // console.log(i);
+       // break
+     // }
+ //}
+
+ // task 10
+
+ 
+
+
+
+
+
+
+
+
+ 
+
+ 
