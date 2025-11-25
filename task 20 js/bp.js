deb3c0f588e75a2e5db1c53240540f785d82af25
suryyaramazanova5-eng <suryyaramazanova5@gmail.com>
2025-11-25 20:25:13 +0400

    js

---
+++ task 20 js/bp.js
@@ -0,0 +1,42 @@
+//task 1
+//let list = document.querySelector(".first")
+//console.log(list.textContent);
+
+//task 2
+
+//let head = document.querySelector(".h1")
+//head.remove()
+//let style = document.querySelector(".list")
+//let second = document.createElement("li")
+//second.textContent= "TITAN RTX"
+//style.appendChild(second)
+
+//task 3
+//let text = document.querySelector(".h1")
+//text.textContent="GeForce 20 Series "
+
+//let color = document.querySelectorAll(".primary")
+//for (let i = 0; i < color.length; i++) {
+  //   color [i].style.color= "#ebebeb"
+     
+//}
+
+
+//task 4
+// let own = document.getElementById("btn1")
+ //let own1 = document.getElementById("btn2")
+ //console.log(own.innerHTML);
+ //console.log(own1.innerHTML);
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
