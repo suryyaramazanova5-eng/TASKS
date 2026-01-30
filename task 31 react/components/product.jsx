f5c719682d40df43b9fbbb89338461903bfb7b6a
suryyaramazanova5-eng <suryyaramazanova5@gmail.com>
2026-01-31 00:16:20 +0400

    fetch

---
+++ task 31 react/components/product.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Product({ product }) {
+    return (
+        <div>
+            <img src={product?.thumbnail} alt="product" />
+            <h2 className='text-[violet] text-[24px]'>{product?.title}</h2>
+            <p className='text-[violet] text-[20px]'>{product?.price}AZN</p>
+            <p className='text-[purple]  p-3 '>{product?.description}</p>
+            <button className='border-2 border-[purple] p-2 bg-[purple] text-[pink] rounded-3xl cursor-pointer'>Add to card</button>
+        </div>
+    )
+}
+
+export default Product
+ 
